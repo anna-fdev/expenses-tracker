1fdf5ec1eac79a1091878664b822f5d37fe161b4
Anna Petrychko <annyshka.vin@gmail.com>
2024-01-09 23:32:12 +0200

    Added Header and Footer components, Sign Up and Sign In Pages using MUI

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,50 +1,24 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
-
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Header from '../components/Header';
+import Home from '../pages/Home';
+import SignUp from '../pages/SignUp';
+import SignIn from '../pages/SignIn';
+import Footer from '../components/Footer';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="client" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
       <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
+        <Header />
       </div>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Routes>
+      <div role="footer">
+        <Footer />
+      </div>
       {/* END: routes */}
     </div>
   );
