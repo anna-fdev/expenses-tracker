6230b18e1553ac541c96c8611163e8c08dd20d1c
Anna Petrychko <annyshka.vin@gmail.com>
2024-01-15 22:55:39 +0200

    Configuring Prisma setup

---
+++ apps/server/src/prisma/migrations/20240115210007_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'CLIENT');
+
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+    "username" VARCHAR(45) NOT NULL,
+    "email" VARCHAR(45) NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'CLIENT',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_username_key" ON "Users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
